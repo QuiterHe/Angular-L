bb90c09d4423f8c40a442c72c9920c13847a06a8
zhang.he <zhang.he@wenba100.com>
2017-07-27 17:08:28 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { AppComponent }  from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HeroDetailComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
